23527b7185aa3c8f1b49cd40afff978ecbf93065
bigyandulal2 <136171774+bigyandulal2@users.noreply.github.com>
2023-09-07 09:00:55 +0545

    Add files via upload

--- Tree.c
+++ Tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<stdlib.h>
+int count=0;
 struct Node{
 int data;
 struct Node *prev,*next;
@@ -27,11 +28,11 @@ struct Node *insert(struct Node *head,int pos,int key){
       }
     }
     if(tail->next!=NULL){
-   tail->next->prev=newnode;
-    newnode->next=tail->next;
-    tail->next=newnode;
-    newnode->prev=tail;
-    tail=newnode;
+      tail->prev->next=newnode;
+      newnode->prev=tail->prev;
+      newnode->next=tail;
+      tail->prev=newnode;
+      tail=newnode;
     return head; 
   }
     tail->next=newnode;
@@ -40,7 +41,43 @@ struct Node *insert(struct Node *head,int pos,int key){
     return head;    
   }
 }
+struct Node *delete(struct Node *head,int pos){
+  if(head==NULL){
+    printf("linked list is empty");
+    return head;
+  }
+  else{
+    struct Node *tail=head;
+    if(pos==1){
+        head=head->next;
+        head->prev=NULL;
+        free(tail);
+       return head;
+    }
+    for(int i=1;i<pos;i++){
+     if(tail->next!=NULL){
+       tail=tail->next;
+     }
+    }
+   if(tail->next!=NULL){
+     tail->prev->next=tail->next;
+     tail->next->prev=tail->prev;
+     free(tail);
+     return head;
+   }
+    if(count>=pos){
+    tail->prev->next=NULL;
+    free(tail);
+    return head;}
+    else{
+      printf("there are only %d linked list\n ",count);
+      printf("but you have %d pos  which is  more than %d linked list\n",pos,count);
+      return head;
+    }   
+  }
+}
 void display(struct Node *head){
+  
   if(head==NULL){
     printf("Node is empty\n");
     return;
@@ -50,15 +87,20 @@ void display(struct Node *head){
   while(head!=NULL){
     printf("%d\t",head->data);
     head=head->next;
+    count++;
   }
+  printf("\n");
 }
 int main(void) {
  struct Node *head=NULL;
- head=insert(head,2,43);
- head=insert(head,3,10);
+ head=insert(head,1,43);
+ head=insert(head,1,10);
  head=insert(head,4,34);
- head=insert(head,1,26);
+ head=insert(head,2,26);
  head=insert(head,10,89);
 display(head);
+  printf("after deleting linked list, outputs are \n");
+  head=delete(head, 15);
+  display(head);
   return 0;
 }
